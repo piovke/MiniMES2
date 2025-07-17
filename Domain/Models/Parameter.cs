af9ffe0e10096798acc7f0bdc3518982b431eeba
Paweł <limek008@gmail.com>
2025-07-17 13:13:14 +0200

    Revert "initial"
    This reverts commit e631fe072c9a6d3b1f5c009e54b0a8fd986f3973.

--- Domain/Models/Parameter.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Models;
-public class Parameter
-{
-    public int Id;
-    public string Name {get; set;} = "";
-    public string Unit { get; set; }="";
-    
-    public ICollection<ProcessParameters> ProcessParameters { get; set; } = new List<ProcessParameters>();
-}
--- Frontend/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Frontend;
-
-public class Class1
-{
-}
